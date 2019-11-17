c0ba2c894d2fca3070629f56b9a8ca9ca64761d3
Dmitriy Tyulpa <dimonakainnocent@gmail.com>
2019-11-17 22:29:22 +0200

    cssmin add

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,26 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
 
+        cssmin: {
+            options: {
+                sourceMap: true,
+                mergeIntoShorthands: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    'pub/css/responsive-styles.min.css': [
+                        'assets/dist/css/reset.css',
+                        'assets/dist/css/responsive-styles.css'
+                    ]
+                }
+            }
+        }
+
     });
 
-    grunt.registerTask('default', []);
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.registerTask('default', ['cssmin']);
 
 };
